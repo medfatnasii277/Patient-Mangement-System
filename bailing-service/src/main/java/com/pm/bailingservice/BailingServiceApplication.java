047c9096d72bfbbc0b197cd021fd0f30bfadb2cb
medfatnasii277 <mohamedfatnassi277@gmail.com>
2025-06-14 23:32:22 +0100

    baillling service + grpc setup

---
+++ bailing-service/src/main/java/com/pm/bailingservice/BailingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.bailingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BailingServiceApplication.class, args);
+	}
+
+}
---
+++ bailing-service/src/test/java/com/pm/bailingservice/BailingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.bailingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
